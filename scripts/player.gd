497ae27fbda4fc440f8f5ab8846ed11c0435b56b
Mickael MESLOUB <mickael.mesloub2025@campus-eni.fr>
2026-03-16 15:38:12 +0100

    add coyote time to improve jumping fluidity

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,23 +3,32 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+
+# Used for coyote time to allow character to jump even if he's not on the ground anymore for a few frames
+var is_falling = false
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_time: Timer = $CoyoteTime
 
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or !coyote_time.is_stopped():
+			velocity.y = JUMP_VELOCITY
+		
+	if !is_on_floor() and !is_falling:
+		coyote_time.start()
+		is_falling = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	# Get the input direction: -1, 0 or 1
 	var direction := Input.get_axis("move_left", "move_right")
-	
 	
 	# Flip the sprite
 	if direction > 0:
@@ -29,6 +38,7 @@ func _physics_process(delta: float) -> void:
 	
 	# Play animations
 	if is_on_floor():
+		is_falling = false  
 		if direction == 0:
 			animated_sprite.play("Idle")
 		else: 
